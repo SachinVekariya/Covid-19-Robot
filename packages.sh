85b3f6595cc6869f3c1c0db66e5b6fc25c2f96f2
SachinVekariya <46790387+SachinVekariya@users.noreply.github.com>
2020-05-16 16:51:31 +0530

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install git-core
-#sudo apt-get install ros-melodic-rosserial-arduino ros-melodic-rosserial-python ros-melodic-rosserial-server ros-melodic-rosserial-client ros-melodic-rosserial-msgs
+sudo apt-get install ros-melodic-rosserial-arduino ros-melodic-rosserial-python ros-melodic-rosserial-server ros-melodic-rosserial-client ros-melodic-rosserial-msgs
 
 cd ~/catkin_ws/src
 git clone https://github.com/SachinVekariya/driving.git
